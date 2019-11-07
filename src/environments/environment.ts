e8218b06625757ea54cb0f83a0517971d153f063
Keith Rose <37423459+KeithAndreRose@users.noreply.github.com>
2019-11-06 21:10:32 -0500

    service key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCdU_SEAVg39G9fsgk-aXc94Rq9eTeJUSM",
-    authDomain: "game-plug.firebaseapp.com",
-    databaseURL: "https://game-plug.firebaseio.com",
-    projectId: "game-plug",
-    storageBucket: "game-plug.appspot.com",
-    messagingSenderId: "800533028318",
-    appId: "1:800533028318:web:af5af676882f7cee"
-  }
+  firebaseConfig: null
 };
 
 /*
